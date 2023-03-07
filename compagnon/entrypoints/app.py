1c788462700c59ab816a6b8aa83ab82453f874f2
ckaipf <camill@posteo.de>
2023-03-07 16:43:08 +0100

    update app commands

--- compagnon/entrypoints/app.py
+++ compagnon/entrypoints/app.py
@@ -5,22 +5,26 @@ app = typer.Typer()
 import compagnon.service_layer.batchables as batchables
 from compagnon.fetchers.fetchers import CogdatFetcher
 from compagnon.service_layer.unit_of_work import YamlUnitOfWork
-
-
+import compagnon.service_layer.services as services
+from compagnon.service_layer.executions.kraken import KrakenExecution
 @app.command()
 def test():
+    uow = YamlUnitOfWork("test.yml")
     batchables.add_missing_records_from_remote(
-        YamlUnitOfWork("test.yml"), CogdatFetcher()
+        uow, CogdatFetcher()
     )
+    services.add_execution_to_records(
+        KrakenExecution,
+        uow,
+    )
+    batchables.execute_executions(uow)
     return
 
 
 @app.callback()
 def main():
     """
-    Format validation.
     """
-
 
 if __name__ == "__main__":
     app.app()
